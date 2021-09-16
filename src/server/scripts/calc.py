d139ee00c2cef0f9af197999cc642ea730055910
alexx sub <alexei.s@list.ru>
2021-09-16 11:48:56 +0300

    fixed

--- src/server/scripts/calc.py
+++ src/server/scripts/calc.py
@@ -1,25 +1,32 @@
-import sys, argparse,time,locale
+import sys,argparse,time,locale
 version = "1.0.0"
-
 parser = argparse.ArgumentParser(
     prog = 'calc',
     description = '''Это простая программа, которая выводит сумму a и b, a+b=''',
-    epilog = '''© aLexx Sub ☭ , 2021. Автор программы, как всегда,
+    epilog = '''© aLexx Sub, 2021. Автор программы, как всегда,
 не несет никакой ответственности ни за что.''',
     add_help = False
 )
 
 base_group = parser.add_argument_group (title='Обязательные параметры')
-base_group.add_argument("-a", type=int, help="первое слагаемое a")
-base_group.add_argument("-b", type=int, help="второе слагаемое b")
+base_group.add_argument("-a", help="первое слагаемое a")
+base_group.add_argument("-b", help="второе слагаемое b")
 optional_group = parser.add_argument_group (title='Дополнительные параметры')
 optional_group.add_argument ('--help', '-h', action='help', help='Показывает эту помощь')
 optional_group.add_argument ('--version','-v',
             action='version',
             help = 'Вывод текущей версии',
             version='Текущая версия %(prog)s  {}'.format (version))
+try:
+    args = parser.parse_args()
+except:
+    parser.print_help()
+    sys.exit(0)
 
-args = parser.parse_args()
+if len(sys.argv)==1:
+    parser.print_help()
+    sys.exit(0)
+
 locale.setlocale(locale.LC_TIME, "ru_ru")
 print("Старт : %s" % time.strftime("%a, %d %b %Y %H:%M:%S"))
 time.sleep( 1 )
